b02714e127ee30b44787b24dd802b29b42b2f20b
buchuleaf <koolaidonme@gmail.com>
2025-09-29 22:37:25 -0700

    clean up

--- src/index.tsx
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-import React from 'react';
-import { render } from 'ink';
-import CLI from './cli.js';
-
-const programRoot = process.cwd();
-
-render(<CLI programRoot={programRoot} />);
